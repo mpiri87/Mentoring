56b4598de8528cad41da89392dbb0a7808f441d9
Memoleee <piri.mehmet@googlemail.com>
2021-03-13 16:06:54 +0100

    function arguments
    Co-authored-by: Mostafa Othman <smogeq7@yahoo.com>

--- Javascript /mentoring_100321/functions/app.js
+++ Javascript /mentoring_100321/functions/app.js
@@ -103,3 +103,130 @@ console.log(`r4 ====> ${r4}`);
 console.log(`r5 ====> ${r5}`);
 console.log(`r6 ====> ${r6}`);
 console.log(`r7 ====> ${r7}`); */
+
+// 130321 Mentoring MOstafa
+
+/* Define a function which will take name a string as an argument, and it will return nothing
+ * it shoul only console.log Hello "the name as we pass it as an argument"
+ * EX: Hello Memet
+ */
+
+/* function hallo(name) {
+  // void function "return nothing" no return
+  console.log(`Hello ${name}`);
+}
+
+hallo("Mehmet");
+hallo("Mostafa");
+
+/**
+ * This Function To return Sum of Values.
+ * @author Memet
+ * @param {Number} a as a number // it take a as a arguments
+ * @param {Number} b as a number
+ * @returns {Number} sum a and b
+ */
+/* function sum(a, b) {
+  return a + b;
+}
+
+let x = sum(6, 4);
+console.log(x); */
+
+/**
+ * This variable is important for me
+ * test test
+ */
+// const someVar = "normal text";
+
+// 6  7   10  ====> 23/3
+/* function durchschnitt() {
+  // arguments is a keyword inside a function it will read all arguments and put it inside an array
+  sum = 0;
+  for (i = 0; i < arguments.length; i++) {
+    console.log("i ist:" + i);
+    console.log("dann value ist: " + arguments[i]);
+    sum += arguments[i];
+  }
+
+  // console.log(arguments)
+  // return (a + b + c) / 3;
+  return sum / arguments.length;
+}
+console.log(durchschnitt(6, 7, 8, 10, 2, 6, 2)); */
+
+/* function avarage() {
+  sum = 0;
+  for (i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+
+  return sum / arguments.length;
+}
+console.log(avarage(5, 6, 7, 8, 9, 100)); */
+
+// check only number
+function avarage() {
+  sum = 0;
+  let zahlenCount = arguments.length;
+  for (i = 0; i < arguments.length; i++) {
+    console.log(
+      `i is: "${i}",and value is: "${
+        arguments[i]
+      }", und Type of this element ist: ${typeof arguments[i]} `
+    );
+    // typeof "variable" ===> typeof this variable as a string result
+    if (typeof arguments[i] === "number") {
+      sum += arguments[i];
+    } else {
+      zahlenCount--;
+    }
+  }
+
+  return sum / zahlenCount;
+}
+console.log(avarage(5, 6, "g", 8, 9, 100, "d")); // ===> object as a string
+
+function argumentsAnalyser() {
+  let countOfNumbers = 0;
+  let countOfStrings = 0;
+  let countOfObjects = 0;
+  let countOfBooleans = 0;
+
+  for (i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] === "number") {
+      countOfNumbers++;
+    } else if (typeof arguments[i] === "string") {
+      countOfStrings++;
+    } else if (typeof arguments[i] === "object") {
+      countOfObjects++;
+    } else {
+      countOfBooleans++;
+    }
+  }
+  return {
+    countOfNumbers: countOfNumbers,
+    countOfStrings: countOfStrings,
+    countOfObjects: countOfObjects,
+    countOfBooleans: countOfBooleans,
+  };
+}
+
+console.log(
+  argumentsAnalyser(
+    4,
+    5,
+    8,
+    "sfgh",
+    true,
+    [3, 5, 8],
+    { l: "g" },
+    false,
+    40,
+    "dfgj"
+  )
+);
+
+console.log(argumentsAnalyser(2));
+
+// to read all arguments inside a function use "arguments" ===>[]
